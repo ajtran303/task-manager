580646057e9f880161343dcde73351151a2c08ff
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-08-29 11:47:15 -0400

    Add laundry? method to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-
+  def laundry?
+    if title.split.map(&:downcase).include? 'laundry'
+      return true
+    elsif description.split.map(&:downcase).include? 'laundry'
+      return true
+    else
+      return false
+    end
+  end
 end
